96797448dda4d0f46798627f6aca613953e8e385
gagelo01 <eloi.gagnon.1@ulaval.ca>
2023-01-19 11:16:08 -0500

    remove susie analysis

--- 2b_extract_sign.R
+++ 2b_extract_sign.R
@@ -9,6 +9,7 @@ setwd("/mnt/sda/gagelo01/Projects/Brain_pQTL")
 res_all <- readRDS( "Data/Modified/safely_res_all5.rds")
 index <- sapply(res_all, function(x) !is.null(x$result))
 dt_res <- lapply(res_all[index], function(x) x$result) %>% rbindlist(., fill = TRUE)
+dt_gene_region <- fread( "Data/Modified/dt_gene_region.txt")
 
 ##########MAP###############
 
@@ -32,7 +33,6 @@ if(!file.exists("Data/Modified/BMI_loci.txt")) {
 
 ###############results of the PWAS####################
 #report if results are in the ABO, APOE, HLA-A gene region
-setnames(dt_res, c("chr.exposure.wald","pos.exposure.wald"), c("chr.exposure", "pos.exposure"))
 to_exclude = c("APOE", "ABO", "HLA-A")
 window = 2e+06
 gencode <- fread("/home/couchr02/Mendel_Commun/Nicolas/GTEx/gencode.v19.genes.v7.patched_contigs.txt")
@@ -46,20 +46,20 @@ for (i in 1:length(to_exclude)) {
 region_df <- rbindlist(list)
 dt_res[, is_in_pleiotropic_region := FALSE]
 for (i in 1:nrow(region_df)) {
-  dt_res[(chr.exposure == region_df[i, ]$chr) & 
-           (pos.exposure >= region_df[i, ]$start) & (pos.exposure <= 
+  dt_res[(chr.exposure.wald == region_df[i, ]$chr) & 
+           (pos.exposure.wald >= region_df[i, ]$start) & (pos.exposure.wald <= 
                                                        region_df[i, ]$end), is_in_pleiotropic_region := TRUE]
 }
 
 
-#k
-dt_gene_region <- fread( "Data/Modified/dt_gene_region.txt")
+#modify dt_res to include hgnc symbol
 dt_aptamers <- as.data.table(readat::aptamers)
 dt_res <- merge(dt_res,dt_aptamers[, .(AptamerId,UniProt) ], by.x = "exposure", by.y = "AptamerId", all.x = TRUE)
 dt_res[study != "yang", UniProt := exposure]
-dt_res <- merge(dt_res, dt_gene_region[, .(trait, id, gene_region, hgnc, study)],
+dt_res <- merge(dt_res, dt_gene_region[, .(trait, id, gene_region, hgnc, ensg, study)],
                 by.x = c("exposure", "study"), by.y = c("trait", "study"), all.x = TRUE)
 setnames(dt_res, "hgnc","hgnc_symbol")
+
 ##results of the mapping
 gene_region <- separate(dt_gene_region, col = "gene_region", into = c("chr", "start", "end"))
 gene_region[, c("chr", "start", "end") := lapply(.SD, as.numeric), .SDcols = c("chr", "start", "end")]
@@ -72,21 +72,42 @@ Nloci <- lapply(gene_region_split, function(bon) {
 Nloci$Nloci %>% table
 
 index_exposure_map <- Nloci[Nloci > 0,]$hgnc %>% unique
-res_map <- dt_res[posprob_coloc_PPH4 > 0.8 & UniProt %in% index_exposure_map, ] #results of the mapping
+res_map <- dt_res[posprob_coloc_PPH4 > 0.8 & hgnc_symbol %in% index_exposure_map, ] #results of the mapping
 
 #results of the pwas
-ntest <- dt_res$UniProt %>% unique %>% length #number of different proteins tested number of test
-res_pwas <- dt_res[pval.wald < 0.05/ntest & posprob_coloc_PPH4 > 0.8 & pval_exposure.wald < 5e-8 & steiger_pval.wald < 0.05 & fstat.exposure.wald > 10,] 
+ntest <- dt_res[!is.na(b.wald), ]$UniProt %>% unique %>% length #number of different proteins tested number of test
+uniprot <- dt_res[pval.wald < 0.05/ntest & posprob_coloc_PPH4 > 0.8  & steiger_pval.wald < 0.05 & fstat.exposure.wald > 10,]$UniProt
+res_pwas <- dt_res[UniProt %in% uniprot,]
 
-###susiesign
-nsnpcoloc <-dt_res[,  apply(.SD, 1, function(x) sum(x>0.8, na.rm = TRUE)), .SDcols = colnames(dt_res)[grepl("PP.H4.abf", colnames(dt_res))], by = c("exposure", "study")][order(exposure)]
-setnames(nsnpcoloc, "V1", "nsnpsusiecoloc")
-dt_res <- merge(dt_res, nsnpcoloc, by = c("exposure", "study"))
+#####Replicatoin rate among uni-cis pwmr
+k1 <- res_pwas[, .N, by = "UniProt"] #replicated
+setnames(k1, "N", "present")
+k2 <- res_pwas[pval.wald < 0.05/ntest & posprob_coloc_PPH4 > 0.8  & steiger_pval.wald < 0.05 & fstat.exposure.wald > 10, .N, by = "UniProt"] #replicated
+setnames(k2, "N", "replicate")
+k <- merge(k1,k2, by = "UniProt")
+k[,ratio := replicate/present]
+k[present == 1,category := "could not replicate"]
+k[present>1 & ratio ==1, category := "replicated"]
+k[present>1 & ratio<1, category := "not replicated"]
+dt_replication_brainpqtl <- k
 
-#multi-cis
-susiesign <- dt_res[pval.multi_cis < 0.05/ntest & nsnpsusiecoloc > 0   & cochranQpval.multi_cis > 0.05,]
+####top cis
+gwasvcf::set_bcftools()
+gwasvcf::set_plink()
+arguments <- dt_res[!is.na(b.wald),.(id.exposure, lead_snp.wald)]
+arguments <- arguments[lead_snp.wald!="",]
+options(future.globals.maxSize= 5e9)
+plan(multisession, workers = 20, gc = TRUE) #plan(sequential)
+
+top_cis <-  future_map(split(arguments, 1:nrow(arguments)), function(x) {
+  dat_tsmr <- gwasvcf::query_gwas(vcf = paste0("/mnt/sda/gagelo01/Vcffile/Server_vcf/", x$id.exposure, "/", x$id.exposure, ".vcf.gz"),
+                                  rsid = x$lead_snp.wald) %>% gwasglue::gwasvcf_to_TwoSampleMR(.) %>% 
+    as.data.table(.)
+  return(dat_tsmr)}, .options = furrr_options(seed = TRUE)) %>% rbindlist(., fill=TRUE)
+
+fwrite(top_cis,  "Data/Modified/top_cis.txt")
+fwrite(dt_replication_brainpqtl, "Data/Modified/dt_replication_brainpqtl.txt")
 fwrite(res_map, "Data/Modified/res_map.txt")
-fwrite(susiesign, file = "Data/Modified/susiesingstringent.txt" )
 fwrite(res_pwas, "Data/Modified/res_pwas.txt")
 fwrite(dt_res, "Data/Modified/dt_res_pwas.txt")
 
